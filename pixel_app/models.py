5b60e9772bd372c937a5622eb24be9fb10407a58
cherucole <cheruyotcheruyot@gmail.com>
2018-10-04 11:49:44 +0300

    working on models

--- pixel_app/models.py
+++ pixel_app/models.py
@@ -4,16 +4,31 @@ from tinymce.models import HTMLField
 # Create your models here.
 
 class User(models.Model):
-    name = models.CharField(max_length=30)
+    username = models.CharField(max_length=30)
     bio = HTMLField()
     email= models.EmailField()
     avatar = models.ImageField(upload_to='images/', blank=True)
 
-    phone_number = models.CharField(max_length = 10,blank =True)
+    def __str__(self):
+        return self.username
+
+    def save_editor(self):
+        self.save()
+
+    class Meta:
+        ordering = ['username']
+
+class Comment(models.Model):
+    comment = models.CharField(max_length=30)
+    user_id = models.ForeignKey(User,on_delete=models.CASCADE)
+    post_id = models.ForeignKey(Post,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     image = models.ImageField(upload_to='images/', blank=True)
-    name = models.CharField(max_length=30)
+    # name = models.CharField(max_length=30)
     caption = HTMLField()
     likes=models.IntegerField()
     comments = models.ForeignKey(Comment,on_delete=models.CASCADE)
@@ -24,10 +39,4 @@ class Post(models.Model):
         images = cls.objects.all()
         return images
 
-class Comment(models.Model):
-    comment = models.CharField(max_length=30)
-    user_id = models.ForeignKey(User,on_delete=models.CASCADE)
-    post_id = models.ForeignKey(Post,on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.name
