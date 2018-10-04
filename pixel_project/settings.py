d07a1f7ff8f9231b46bb8c779d429f8f9eea7369
cherucole <cheruyotcheruyot@gmail.com>
2018-10-04 12:48:38 +0300

    added link to database correctly

--- pixel_project/settings.py
+++ pixel_project/settings.py
@@ -114,8 +114,10 @@ WSGI_APPLICATION = 'pixel_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pixels',
+        'USER': 'cherucole',
+    'PASSWORD':'cherucole',
     }
 }
 
