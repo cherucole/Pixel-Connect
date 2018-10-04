40c5d2b6bed41b3cc2da2d72a6cb050c818ddf30
cherucole <cheruyotcheruyot@gmail.com>
2018-10-04 11:56:46 +0300

    still working on the models

--- pixel_app/models.py
+++ pixel_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from tinymce.models import HTMLField
+from .models import *
 
 # Create your models here.
 
@@ -12,11 +13,27 @@ class User(models.Model):
     def __str__(self):
         return self.username
 
-    def save_editor(self):
+    def save_user(self):
         self.save()
 
     class Meta:
         ordering = ['username']
+
+class Post(models.Model):
+    image = models.ImageField(upload_to='images/', blank=True)
+    # name = models.CharField(max_length=30)
+    caption = HTMLField()
+    likes=models.IntegerField()
+    comments = models.ForeignKey(Comment,on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User,on_delete=models.CASCADE)
+
+
+    # pub_date = models.DateTimeField(auto_now_add=True)
+    @classmethod
+    def all_posts(cls):
+        posts = cls.objects.all()
+        return posts
+
 
 class Comment(models.Model):
     comment = models.CharField(max_length=30)
@@ -26,17 +43,7 @@ class Comment(models.Model):
     def __str__(self):
         return self.name
 
-class Post(models.Model):
-    image = models.ImageField(upload_to='images/', blank=True)
-    # name = models.CharField(max_length=30)
-    caption = HTMLField()
-    likes=models.IntegerField()
-    comments = models.ForeignKey(Comment,on_delete=models.CASCADE)
-
-    # pub_date = models.DateTimeField(auto_now_add=True)
-    @classmethod
-    def all_images(cls):
-        images = cls.objects.all()
-        return images
-
+class Likes(models.Model):
+	post_id = models.IntegerField()
+	liker = models.CharField(max_length=20)
 
